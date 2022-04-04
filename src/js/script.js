4d56bac8a95c0f1c7f8fbb54545df24b91b903c3
Antonov Vyacheslav <islava787@gmail.com>
2022-04-04 18:28:31 +0300

    adding 4section slick slider

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+      speed: 1000,
+      prevArrow: '<button type="button" class="slick-prev"><img src ="icons/left.svg"</button>',
+      nextArrow: '<button type="button" class="slick-next"><img src ="icons/right.svg"</button>',
+      responsive:[
+        {
+          breakpoint: 1090,
+          settings: {
+            dots: true,
+            arrows: false
+          }
+        },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+        }
+      ]
+    });
+  });
+  console.log("here");
+      
